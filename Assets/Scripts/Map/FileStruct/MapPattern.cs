cc35f0ebf37715008709c85c1ab9e4e8563ce9bd
Hoang Tien Minh <htminh.1001@gmail.com>
2023-03-07 08:53:44 +0700

    feat: add map pattern and cohesion setting files, add basic file struct to load files

---
+++ Assets/Scripts/Map/FileStruct/MapPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class MapPattern
+{
+	// To identify current pattern and to determine their next pattern
+	public int id;
+
+	// Map length
+	public int length;
+
+	// Data convertible to a list objects and their position
+	public int[] data;
+}
+
---
+++ Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    private MapPatternFile _mapPatterns;
+
+    private MapCohesionFile _mapCohesions;
+
+    private void Awake()
+    {
+        ICommand loadMapPatternCommand = new LoadMapPattern(SetMapPatterns);
+        ICommand loadMapCohesionCommand = new LoadMapCohesion(SetMapCohesions);
+
+        loadMapCohesionCommand.Execute();
+        loadMapCohesionCommand.Execute();
+    }
+
+    private void SetMapPatterns(MapPatternFile mapPatterns)
+    {
+        _mapPatterns = mapPatterns;
+    }
+
+    private void SetMapCohesions(MapCohesionFile mapCohesions)
+    {
+        _mapCohesions = mapCohesions;
+    }
+}
+

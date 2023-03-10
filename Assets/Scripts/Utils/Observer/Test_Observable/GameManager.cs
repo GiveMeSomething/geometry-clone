e33e38ba7e3cd270ad2c74c7625f79c26f07a777
meof-coding <tranthe150186@fpt.edu.vn>
2023-03-10 15:56:05 +0700

    feat: Add Observable for Observer pattern and implement game over screen
    - Add Observable class to implement the Observer design pattern for detecting jumps and game over events
    - Modify JumpDetector to use the Observable class for events
    - Create a new GameOverScreen script to show the game over screen with a restart button
    - Modify GameManager to use the new GameOverScreen script and subscribe to the jumpDetector events
    - Create a new scene to test the changes
    
    BREAKING CHANGE: None

---
+++ Assets/Scripts/Utils/Observer/Test_Observable/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public GameOverScreen gameOverScreen;
+    public JumpDetector jumpDetector;
+
+    private void Start()
+    {
+        jumpDetector = FindObjectOfType<JumpDetector>();
+        jumpDetector.onGameOver.Subscribe(OnObstacleHit);
+    }
+
+    private void OnObstacleHit(bool hit)
+    {
+        Time.timeScale = 0;
+        gameOverScreen.ShowGameOverScreen();
+    }
+}
+
---
+++ Assets/Scripts/Utils/Observer/Test_Observable/GameOverScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UIElements;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void ShowGameOverScreen()
+    {
+        gameObject.SetActive(true);
+    }
+}
+

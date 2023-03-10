0b377ce3ebb54d42f409851b126af955c3f04d33
nguyenminhsonn <sown2432001@gmail.com>
2023-03-10 19:02:46 +0700

    fix comment

--- Assets/Scripts/Player/State/PlayerState.cs
+++ Assets/Scripts/Player/State/PlayerState.cs
@@ -6,14 +6,12 @@ public abstract class PlayerState
 {
     protected PlayerBehaviour _playerBehaviour;
     protected GameObject _gameObject;
-    protected GameObject _childGameObject;
 
     public void SetContext(PlayerBehaviour behaviour)
     {
-        this._playerBehaviour = behaviour;
-        this._gameObject = behaviour.gameObject;
-        this._childGameObject = GameObject.FindGameObjectWithTag("PlayerSlab");
-        this.SetUpEnviroment();
+        _playerBehaviour = behaviour;
+        _gameObject = behaviour.gameObject;
+        SetUpEnviroment();
     }
     public abstract void SetUpEnviroment();
     public abstract void HandleUserSingleTouch();

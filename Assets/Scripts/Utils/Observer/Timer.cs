445ad3cdba7a0f214908b0ee180e2d9080165205
meof-coding <tranthe150186@fpt.edu.vn>
2023-03-13 21:00:34 +0700

    feat: add async support to Timer class

---
+++ Assets/Scripts/Utils/Observer/Timer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Timer
+{
+    public static async Task SetTimeout(Action action, float time)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(time));
+        action();
+    }
+}
